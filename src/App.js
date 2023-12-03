6c00f5b6c5378ba8b1737d8e8b5140c1f4a85c33
guna <gunario007@gmail.com>
2023-12-03 17:28:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
